abdd9306187a23380c335222f97fe52f97ded034
MassZwaker <maheshsuresh471@gmail.com>
2023-04-24 05:31:33 +0530

    update in skeliton.py

--- skeliton.py
+++ skeliton.py
@@ -17,10 +17,10 @@ def profile():
     input_dict = {
         "profile": [
             {
-                "column": "PassengerId",
+                "column": "",
                 "attributeSummary":
                     {
-                    "records": 418,
+                    "records": 0,
                     "dataType": "Numeric",
                     "null_records": 0,
                     "outliers": 0,
@@ -122,19 +122,19 @@ def profile():
                     }
                 ],
                 "dq": {
-                    "ColumnName": "PassengerId",
+                    "ColumnName": "",
                     "detail": {
                         "Completeness": {
-                            "value": "100.0",
+                            "value": "",
                             "info": [
                                 {
-                                    "rule": "Null check",
-                                    "OutlierCount": "0"
+                                    "rule": "",
+                                    "OutlierCount": ""
                                 }
                             ]
                         },
                         "Validity": {
-                            "value": "100.0",
+                            "value": "",
                             "info": [
                                 {
                                     "rule": "DataType match check (DataType : Numeric) ",
@@ -151,7 +151,7 @@ def profile():
                             ]
                         },
                         "Consistenecy": {
-                            "value": "100.0",
+                            "value": "",
                             "info": [
                                 {
                                     "rule": "No Rules Executed",
@@ -160,7 +160,7 @@ def profile():
                             ]
                         },
                         "Timeliness": {
-                            "value": "100.0",
+                            "value": "",
                             "info": [
                                 {
                                     "rule": "file reception frequency check",
@@ -169,7 +169,7 @@ def profile():
                             ]
                         },
                         "Accuracy": {
-                            "value": "100.0",
+                            "value": "",
                             "info": [
                                 {
                                     "rule": "No Rules executed",
@@ -178,380 +178,14 @@ def profile():
                             ]
                         }
                     },
-                    "overall": "100.0"
+                    "overall": ""
                 }
             },
-            {
-                "column": "Pclass",
-                "attributeSummary": {
-                    "records": 418,
-                    "dataType": "Numeric",
-                    "null_records": 0,
-                    "outliers": 0,
-                    "duplicates": 0,
-                    "invalid": 0
-                },
-                "valueStatistics": {
-                    "MinimumValue": 1,
-                    "MinValLength": 1,
-                    "MaximumValue": 3,
-                    "MaxValLength": 1,
-                    "MeanValue": 2.27,
-                    "MedianValue": 3,
-                    "UniqueValuesCount": 3,
-                    "Std_Dev": 0.84
-                },
-                "LengthStatistics": {
-                    "Min": 1,
-                    "Max": 1,
-                    "Average": 1,
-                    "Median": 1
-                },
-                "frequncyAnalysis": [
-                    {
-                        "unique_values": 3,
-                        "counts": 218
-                    },
-                    {
-                        "unique_values": 1,
-                        "counts": 107
-                    },
-                    {
-                        "unique_values": 2,
-                        "counts": 93
-                    }
-                ],
-                "patternAnalysis": [],
-                "maskAnalysis": [],
-                "staticalAnalysis": {
-                    "Count": 418,
-                    "NullCount": 0,
-                    "UniqueValuesCount": 3,
-                    "MinimumValue": 1,
-                    "MeanValue": 2.27,
-                    "MedianValue": 3,
-                    "MaximumValue": 3,
-                    "Std_Dev": 0.84,
-                    "minLength": 1,
-                    "MaxLength": 1,
-                    "MeanLength": 1,
-                    "MedianLength": 1,
-                    "Data Type": "Numeric",
-                    "suggested_dtype": "Numeric"
-                },
-                "outliersList": [],
-                "outliersPercent": {
-                    "outliers": 0,
-                    "normal": 100
-                },
-                "isFrequencyChart": "True",
-                "correlationSummary": {
-                    "positiveSummary": [
-                        {
-                            "column": "Parch",
-                            "value": 0.02
-                        },
-                        {
-                            "column": "SibSp",
-                            "value": 0
-                        }
-                    ],
-                    "negativeSummary": []
-                },
-                "datacatalogue": [
-                    {
-                        "Column": "Pclass",
-                        "id": "c2",
-                        "BusinessTerm": "Ticket class",
-                        "Definition": " 1 = 1st, 2 = 2nd, 3 = 3rd ; A proxy for socio-economic status (SES) 1st = Upper,2nd = Middle,3rd = Lower",
-                        "Classification": "Categorial",
-                        "DataDomain": "Travel",
-                        "RelatedTerms": "",
-                        "RelatedSource": "",
-                        "RelatedReports": "",
-                        "DataOwners": "Vinoth",
-                        "DataUsers": "Admin",
-                        "RelatedTags": "Classification",
-                        "datalineage": [
-                            {
-                                "LineageName": "File Storage",
-                                "Details": {
-                                    "name": "S2S1Titanic.xls",
-                                    "type": "File",
-                                    "id": "S2",
-                                    "metaData": [
-                                        {
-                                            "Column": "Pclass",
-                                            "id": "c2",
-                                            "BusinessTerm": "Ticket class"
-                                        }
-                                    ]
-                                }
-                            },
-                            {
-                                "LineageName": "Extract",
-                                "Details": {
-                                    "name": "S2S1Titanic.xls",
-                                    "type": "Dataset",
-                                    "metaData": [
-                                        {
-                                            "Column": "Pclass",
-                                            "id": "c2",
-                                            "BusinessTerm": "Ticket class"
-                                        }
-                                    ]
-                                }
-                            }
-                        ]
-                    }
-                ],
-                "dq": {
-                    "ColumnName": "Pclass",
-                    "detail": {
-                        "Completeness": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "Null check",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Validity": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "DataType match check (DataType : Numeric) ",
-                                    "OutlierCount": "0"
-                                },
-                                {
-                                    "rule": "Length check (Max Length should be 1) ",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Consistenecy": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "No Rules Executed",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Timeliness": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "file reception frequency check",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Accuracy": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "No Rules executed",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        }
-                    },
-                    "overall": "100.0"
-                }
-            },
-            {
-                "column": "Embarked",
-                "attributeSummary": {
-                    "records": 418,
-                    "dataType": "Alphabetic",
-                    "null_records": 0,
-                    "outliers": 0,
-                    "duplicates": 0,
-                    "invalid": 0
-                },
-                "LengthStatistics": {
-                    "Min": 1,
-                    "Max": 1,
-                    "Average": 1,
-                    "Median": 1
-                },
-                "frequncyAnalysis": [
-                    {
-                        "unique_values": "S",
-                        "counts": 270
-                    },
-                    {
-                        "unique_values": "C",
-                        "counts": 102
-                    },
-                    {
-                        "unique_values": "Q",
-                        "counts": 46
-                    }
-                ],
-                "patternAnalysis": [
-                    {
-                        "unique_values": "W",
-                        "counts": 418
-                    }
-                ],
-                "maskAnalysis": [
-                    {
-                        "unique_values": "L",
-                        "counts": 418
-                    }
-                ],
-                "staticalAnalysis": {
-                    "Count": 418,
-                    "UniqueValuesCount": 3,
-                    "MinimumLength": 1,
-                    "MaximumLength": 1,
-                    "DataType": "string",
-                    "Alphabetic": 418,
-                    "Numeric": 0,
-                    "Alphanumeric": 0,
-                    "NullCount": 0,
-                    "MeanLength": 1,
-                    "MedianLength": 1,
-                    "duplicateCount": 415,
-                    "PercentageAlpha": 100,
-                    "PercentageNum": 0,
-                    "PercentageAlNum": 0,
-                    "suggested_dtype": "Alphabetic"
-                },
-                "outliersList": [],
-                "outliersPercent": {},
-                "isFrequencyChart": "True",
-                "correlationSummary": {
-                    "positiveSummary": [
-                        {
-                            "column": "Ticket",
-                            "value": 1
-                        },
-                        {
-                            "column": "Name",
-                            "value": 1
-                        },
-                        {
-                            "column": "Cabin",
-                            "value": 0.98
-                        },
-                        {
-                            "column": "Sex",
-                            "value": 0.01
-                        }
-                    ],
-                    "negativeSummary": []
-                },
-                "datacatalogue": [
-                    {
-                        "Column": "Embarked",
-                        "id": "c11",
-                        "BusinessTerm": "Port of Embarkation",
-                        "Definition": "C = Cherbourg, Q = Queenstown, S = Southampton",
-                        "Classification": "Categorial",
-                        "DataDomain": "Travel",
-                        "RelatedTerms": "",
-                        "RelatedSource": "",
-                        "RelatedReports": "",
-                        "DataOwners": "Vinoth",
-                        "DataUsers": "Admin",
-                        "RelatedTags": "Classification",
-                        "datalineage": [
-                            {
-                                "LineageName": "File Storage",
-                                "Details": {
-                                    "name": "S2S1Titanic.xls",
-                                    "type": "File",
-                                    "id": "S2",
-                                    "metaData": [
-                                        {
-                                            "Column": "Embarked",
-                                            "id": "c11",
-                                            "BusinessTerm": "Port of Embarkation"
-                                        }
-                                    ]
-                                }
-                            },
-                            {
-                                "LineageName": "Extract",
-                                "Details": {
-                                    "name": "S2S1Titanic.xls",
-                                    "type": "Dataset",
-                                    "metaData": [
-                                        {
-                                            "Column": "Embarked",
-                                            "id": "c11",
-                                            "BusinessTerm": "Port of Embarkation"
-                                        }
-                                    ]
-                                }
-                            }
-                        ]
-                    }
-                ],
-                "dq": {
-                    "ColumnName": "Embarked",
-                    "detail": {
-                        "Completeness": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "Null check",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Validity": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "DataType match check (DataType : alphabets) ",
-                                    "OutlierCount": "0"
-                                },
-                                {
-                                    "rule": "Length check (Max Length should be 1) ",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Consistenecy": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "No Rules Executed",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Timeliness": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "file reception frequency check",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        },
-                        "Accuracy": {
-                            "value": "100.0",
-                            "info": [
-                                {
-                                    "rule": "No Rules executed",
-                                    "OutlierCount": "0"
-                                }
-                            ]
-                        }
-                    },
-                    "overall": "97.17"
-                }
-            }
+
         ],
         "nr_duplicates": 0,
-        "nr_totalrecords": 418,
-        "nr_totalcols": 11
+        "nr_totalrecords": 0,
+        "nr_totalcols": 0
     }
 
     return jsonify(input_dict)
