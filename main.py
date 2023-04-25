48fced9875da6a5d0e947e0481d92d067bd2ee9f
MassZwaker <maheshsuresh471@gmail.com>
2023-04-25 16:00:03 +0530

    update in main.py

--- main.py
+++ main.py
@@ -2,108 +2,229 @@ import findspark
 findspark.init()
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+from pyspark.sql.functions import col, length, min, max, avg, mean
 from pyspark.sql.types import FloatType
-from pyspark.sql.functions import min, max, avg, mean, length, col, count, corr, countDistinct, isnan, when, concat_ws
 from datetime import datetime
 from flask import Flask, request, render_template, send_from_directory, jsonify
 
-
 app = Flask(__name__)
 
-# Create SparkSession
 spark = SparkSession.builder.appName('CSV to JSON').getOrCreate()
 
-# Read CSV file
 df = spark.read.csv('D:/AI-Data-Driven/Documents/Titanic01.csv', header=True, inferSchema=True)
-df_copy = df
-ref_df = df
 
-# To list out the rows and columns of the dataframe
-df.show(10)
-
-df.printSchema()
-
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-    return ("Pyspark")
+    return ("Pyspark ")
 
-@app.route('/printSchema', methods=['GET'])
-# Function to print out the schema of the dataframe, which includes data types of each column
-def get_attribute_summary():
-    return [df.printSchema]
-
-@app.route('/api/profile', methods=['GET'])
+@app.route("/api/profile", methods=['GET'])
 def profile():
-    null_value = {col: df.filter(df[col].isNull()).count() for col in df.columns}
-    min_value = [df.select(F.min(column)).collect()[0][0] for column in df.columns]
-    max_value = [df.select(F.max(column)).collect()[0][0] for column in df.columns]
-    avg_value = [df.select(F.avg(column)).collect()[0][0] for column in df.columns]
-    mean_value = [df.select(F.mean(column)).collect()[0][0] for column in df.columns] 
 
-    values = null_value,min_value,max_value,avg_value,mean_value
-    return jsonify(values)
+    # Get the number of records in the DataFrame
+    num_records = df.count()
 
-@app.route('/api/length', methods=['GET'])
-def get_length():
+    # Get the number of columns in the DataFrame
+    num_columns = len(df.columns)
 
-    col_lengths = df.select([length(c).alias(c) for c in df.columns])
+    # Get the data type of each column
+    column_data_types = [(column, str(data_type)) for column, data_type in df.dtypes]
 
-    results = []
-    for col in col_lengths.columns:
-        row = col_lengths.agg(min(col), max(col), avg(col), mean(col)).collect()[0]
-        result = {
-            "column_name": col,
-            "min_length": row[0],
-            "max_length": row[1],
-            "avg_length": row[2],
-            "mean_length": row[3]
-        }
-        results.append(result)
+    # Get Length statics
 
-    return jsonify(results)
 
-@app.route("/api/frequency", methods=['GET'])
-def frequencies():
-    result = {}
+    # Get the total number of duplicate values for each column
+    duplicates = []
     for column in df.columns:
-        frequency_df = df.groupBy(col(column)).count()
-        result[column] = frequency_df.collect()
-    return jsonify(result)
-
-# @app.route("/api/correlation", methods=['GET'])
-# def calculate_correlations():
-#     # Select only columns with numeric data types
-#     numeric_cols = [col_name for col_name, col_type in df_copy.dtypes
-#                     if isinstance(col_type, (FloatType, IntegerType, DoubleType))]
-#
-#     # Convert selected columns to float data type
-#     for col_name in numeric_cols:
-#         dff = df_copy.withColumn(col_name, F.col(col_name).cast("float"))
-#
-#     # Calculate the correlation value of each column with all other columns
-#     correlations = {}
-#     for col_name in numeric_cols:
-#         if col_name != "id":
-#             corr_values = {}
-#             for other_col in numeric_cols:
-#                 if other_col != col_name:
-#                     corr_val = df.stat.corr(col_name, other_col)
-#                     corr_values[other_col] = corr_val
-#             correlations[col_name] = corr_values
-#
-#     # Create a Pandas DataFrame to display the correlation values
-#     correlation_dff = pd.DataFrame(correlations)
-#     correlation_dff.index.name = "Column"
-#     correlation_dff = correlation_dff.stack().reset_index()
-#     correlation_dff.columns = ["Column 1", "Column 2", "Correlation Value"]
-#
-#     # Convert the DataFrame to a dictionary and return it
-#     return correlation_dff.to_dict(orient="records")
+        num_duplicates = df.groupBy(column).count().filter(col("count") > 1).count()
+        duplicates.append((column, num_duplicates))
 
 
+    input_dict = {
+        "profile": [
+            {
+                "column": "",
+                "attributeSummary":
+                    {
+                    "records": num_records,
+                    "dataType": "",
+                    "null_records": {col: df.filter(df[col].isNull()).count() for col in df.columns},
+                    "outliers": 0,
+                    "duplicates": num_duplicates,
+                    "invalid": 0
+                    },
+                "valueStatics":
+                    {
+                        "MinimumValue": [df.select(F.min(column)).collect()[0][0]],
+                        "MinValLength":0,
+                        "MaximumValue": [df.select(F.max(column)).collect()[0][0]],
+                        "MaxValLength":0,
+                        "MeanValue": [df.select(F.mean(column)).collect()[0][0]],
+                        "MedianValue":[df.select(F.avg(column)).collect()[0][0]],
+                        "UniqueValuesCount": 0,
+                        "Std_Dev":0
+                    },
+                "LengthStatistics":
+                    {
+                        "Min": 0,
+                        "Max": 0,
+                        "Average": 0,
+                        "Median": 0
+                    },
+                "frequncyAnalysis":
+                    {
+                      "unique_values": 0,
+                      "counts": 0
+                    },
+                "patternAnalysis": [],
+                "maskAnalysis": [],
+                "staticalAnalysis":
+                    {
+                        "Count": 0,
+                        "NullCount": 0,
+                        "UniqueValuesCount": 0,
+                        "MinimumValue": [df.select(F.min(column)).collect()[0][0]],
+                        "MeanValue": [df.select(F.mean(column)).collect()[0][0]],
+                        "MedianValue": 0,
+                        "MaximumValue": [df.select(F.max(column)).collect()[0][0]],
+                        "Std_Dev": 0,
+                        "minLength": 0,
+                        "MaxLength": 0,
+                        "MeanLength": 0,
+                        "MedianLength": 0,
+                        "Data Type": "",
+                        "suggested_dtype": ""
+                    },
+                "outliersList": [],
+                "outliersPercent":
+                    {
+                        "outliers": 0,
+                        "normal": 0
+                    },
+                "isFrequencyChart": "",
+                "correlaionSummary":
+                    {
+                        "positiveSummary":
+                            [
+                                {
+                                    "column": "",
+                                    "value": ""
+                                }
+                            ],
+                        "negativeSummary": []
+                    },
+                "datacatalogue": [
+                    {
+                        "Column": "",
+                        "id": "",
+                        "BusinessTerm": "",
+                        "Definition": "",
+                        "Classification": "",
+                        "DataDomain": "",
+                        "RelatedTerms": "",
+                        "RelatedSource": "",
+                        "RelatedReports": "",
+                        "DataOwners": "",
+                        "DataUsers": "",
+                        "RelatedTags": "",
+                        "datalineage":[
+                            {
+                                "LineageName": "",
+                                "Details": {
+                                    "name": "",
+                                    "type": "",
+                                    "id": "",
+                                    "metaData": [
+                                        {
+                                            "column": "",
+                                            "id": "",
+                                            "BusinessTerm": ""
+                                        }
+                                    ]
+                                }
+                            },
+
+                        ]
+                    }
+                ],
+                "dq": {
+                    "ColumnName": "",
+                    "detail": {
+                        "Completeness": {
+                            "value": "",
+                            "info": [
+                                {
+                                    "rule": "",
+                                    "OutlierCount": ""
+                                }
+                            ]
+                        },
+                        "Validity": {
+                            "value": "",
+                            "info": [
+                                {
+                                    "rule": "DataType match check (DataType : Numeric) ",
+                                    "OutlierCount": "0"
+                                },
+                                {
+                                    "rule": "Length check (Min Length should be 3) ",
+                                    "OutlierCount": "0"
+                                },
+                                {
+                                    "rule": "Length check (Max Length should be 4) ",
+                                    "OutlierCount": "0"
+                                }
+                            ]
+                        },
+                        "Consistenecy": {
+                            "value": "",
+                            "info": [
+                                {
+                                    "rule": "No Rules Executed",
+                                    "OutlierCount": "0"
+                                }
+                            ]
+                        },
+                        "Timeliness": {
+                            "value": "",
+                            "info": [
+                                {
+                                    "rule": "file reception frequency check",
+                                    "OutlierCount": "0"
+                                }
+                            ]
+                        },
+                        "Accuracy": {
+                            "value": "",
+                            "info": [
+                                {
+                                    "rule": "No Rules executed",
+                                    "OutlierCount": "0"
+                                }
+                            ]
+                        }
+                    },
+                    "overall": ""
+                }
+            },
+
+        ],
+        "nr_duplicates": num_duplicates,
+        "nr_totalrecords": num_records,
+        "nr_totalcols": num_columns
+    }
+
+    return jsonify(input_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
 
 
 
