5d251a12696c1a95533aa93e0d4000f086b70570
MassZwaker <maheshsuresh471@gmail.com>
2023-06-20 12:39:00 +0530

    profile
    This version of code runs faster

--- profile_api.py
+++ profile_api.py
@@ -24,10 +24,7 @@ from pyspark.sql.types import FloatType
 
 profile_api = Blueprint('profile_api', __name__)
 
-
-@profile_api.route("/api/profile", methods=['POST'])
-def profile():
-    class Connector:
+class Connector:
         def __init__(self):
             self.spark_session = self.get_spark_session()
 
@@ -59,176 +56,118 @@ def profile():
 
             return spark_config
 
+@profile_api.route("/api/profile", methods=['POST'])
+def profile():    
     content = request.get_json()
     sourcepath = content['sourcepath']
+
     df = spark.read.csv(sourcepath, header=True, inferSchema=True)
 
-    # Get the number of records in the DataFrame
+    profileDetail = get_count(df)
+    profileDetail['profile'] = getColumnDetail(df)
 
-    num_records = df.count()
-
-    # Get the number of columns in the DataFrame
-    num_columns = len(df.columns)
-
-    # Get the data type of each column
-    column_data_types = [(column, str(data_type)) for column, data_type in df.dtypes]
-
-    columns = df.columns
-    input_dict = {"profile": []}
-    input_dict['nr_totalrecords'] = num_records
-    input_dict['nr_totalcols'] = num_columns
-
-    row_string = concat_ws(",", *df.columns)
-    df_with_row_string = df.withColumn("row_string", row_string)
-    duplicates = df_with_row_string.groupBy("row_string").agg(count("*").alias("count")).filter(
-        "count > 1").count()
-
-    ListResult = []
-    #
-    selectedColumns = list(df.columns)
-    catalogueresults = getcatalogueforcolumns(selectedColumns)
-    print("Starting the qulaity check")
-    # qualityresult = checkDataQuality(df.head(1000), selectedColumns)
-    print("completed the qulaity check")
-    # c = savequlaitychecktodb(qualityresult, sourcepath)
-    # null_counts = df.select(*(sum(col(c).isNull().cast("int")).alias(c) for c in df.columns))
-    #
-    # total_null_counts = 0
-    #
-    # for row in null_counts.collect():
-    #     for col_name in null_counts.columns:
-    #         total_null_counts += row[col_name]
-
-    print(selectedColumns)
-
-    for column in columns:
-        details = {}
-        attributeSummary = {}
-        LengthStatics = {}
-        valueStatistics = {}
-        frequencyAnalysis = {}
-        staticalAnalysis = {}
-        maskAnalysis = {}
-        patternAnalysis = {}
-        outliersList = {}
-        outliersPercent = {}
-        correlationSummary = {}
-        dq = {}
-
-        num_duplicates = duplicates
-        # print(num_duplicates)
-        null_records = df.filter(df[column].isNull()).count()
-        data_type = str(df.schema[column].dataType)
-        unique_values_count = df.select(approx_count_distinct(column)).first()[0]
-        std_dev = df.select(stddev(column)).first()[0]
-
-        lengths = df.select([length(col(c)).alias(c) for c in df.columns])
-        lengths = lengths.fillna(0)
-        for c in lengths.columns:
-            lengths = lengths.withColumn(c, col(c).cast("int"))
-        length_stats = lengths.agg(
-            *[min(c).alias("Min_" + c) for c in lengths.columns],
-            *[max(c).alias("Max_" + c) for c in lengths.columns],
-            *[avg(c).alias("Average_" + c) for c in lengths.columns]
-        )
-        mean_length = df.select(mean(length(column))).first()[0]
-        # medians = np.median(lengths.toPandas(), axis=0)
-        # median_stats = dict(zip(lengths.columns, medians))
-
-        frequency_analysis = df.groupBy(column).agg(count("*").alias("count")).orderBy(col("count").desc())
-        json_freq = frequency_analysis.toJSON().collect()
-        # print(json_freq)
-
-        attributeSummary['duplicates'] = num_duplicates
-        attributeSummary['null_records'] = null_records
-        attributeSummary['records'] = num_records
-        attributeSummary['outliers'] = 0
-        attributeSummary['invalid'] = 0
-
-        frequencyAnalysis['frequencyAnalysis'] = json_freq
-        # print(frequency_analysis)
-        details['frequencyAnalysis'] = frequencyAnalysis
-
-        maskAnalysis['maskAnalysis'] = []
-        details['maskAnalysis'] = maskAnalysis
-
-        patternAnalysis['patternAnalysis'] = []
-        details['patternAnalysis'] = patternAnalysis
-
-        outliersList['outliersList'] = []
-        details['outliersList'] = outliersList
-
-        outliersPercent['outliersPercent'] = {}
-        details['outliersPercent'] = outliersPercent
-
-        correlationSummary['positiveSummary'] = 0
-        correlationSummary['negativeSummary'] = 0
-        details['correlationSummary'] = correlationSummary
-
-        dq['dq'] = 0
-        details['dq'] = dq
-
-        LengthStatics['Min'] = int(length_stats.first()["Min_" + column])
-        LengthStatics['Max'] = int(length_stats.first()["Max_" + column])
-        LengthStatics['Average'] = round(length_stats.first()["Average_" + column], 2)
-        # LengthStatics['Median'] = int(median_stats[column])
-        details['LengthStatics'] = LengthStatics
-
-        staticalAnalysis['count'] = num_records
-        staticalAnalysis['Nullcount'] = null_records
-        staticalAnalysis['UniqueValuesCount'] = unique_values_count
-        staticalAnalysis['MinimumValue'] = df.select(F.min(column)).collect()[0][0]
-        staticalAnalysis['MeanValue'] = df.select(F.mean(column)).collect()[0][0]
-        staticalAnalysis['MedianValue'] = df.select(F.avg(column)).collect()[0][0]
-        staticalAnalysis['MaximumValue'] = df.select(F.max(column)).collect()[0][0]
-        staticalAnalysis['Std_Dev'] = std_dev
-        staticalAnalysis['minLength'] = int(length_stats.first()["Min_" + column])
-        staticalAnalysis['maxLength'] = int(length_stats.first()["Max_" + column])
-        staticalAnalysis['MeanLength'] = mean_length
-        # staticalAnalysis['MedianLength'] = int(median_stats[column])
-        staticalAnalysis['Data Type'] = data_type
-        staticalAnalysis['suggested_dtype'] = ""
-        details['staticAnalysis'] = staticalAnalysis
-
-        catalogueresult = [x for x in catalogueresults if x['Column'] == column]
-        dcatres = []
-        for eachresult in catalogueresult:
-            eachresult['datalineage'] = dataLineageforcolumn(column)
-            dcatres.append(eachresult)
-        details['datacatalogue'] = dcatres
-        #details['dq'] = [x for x in qualityresult if x['ColumnName'] == column][0]
-
-        if (data_type == "IntegerType()" or data_type == "DoubleType()"):
-            details['column'] = column
-            attributeSummary['dataType'] = "Numeric"
-            valueStatistics["MinimumValue"] = df.select(F.min(column)).collect()[0][0]
-            valueStatistics["MinValLength"] = int(length_stats.first()["Min_" + column])
-            valueStatistics["MaximumValue"] = df.select(F.max(column)).collect()[0][0]
-            valueStatistics["MaxValLength"] = int(length_stats.first()["Max_" + column])
-            valueStatistics["MeanValue"] = df.select(F.mean(column)).collect()[0][0]
-            valueStatistics["MedianValue"] = df.select(F.avg(column)).collect()[0][0]
-            valueStatistics["UniqueValuesCount"] = unique_values_count
-            valueStatistics["Std_Dev"] = std_dev
-            details['valueStatistics'] = valueStatistics
-
-        if (data_type == "datetime64[ns]" or data_type == "datetime"):
-            details['column'] = column
-            attributeSummary['dataType'] = "Alphabetic"
-        if (data_type == "StringType()"):
-            details['column'] = column
-            attributeSummary['dataType'] = "Alphanumeric"
-
-        details['attributeSummary'] = attributeSummary
-        ListResult.append(details)
-    input_dict['profile'] = ListResult
-
-    # Print the input_dict containing the profile information
-    jsonString = json.dumps(input_dict, default=str)
+    jsonString = json.dumps(profileDetail, default=str)
     return jsonString
 
+def get_count(df):
+    count = {}
+    num_records = df.count()
+    num_columns = len(df.columns)
+    grouped_df = df.groupBy(df.columns).count().filter(col("count") > 1)
+    num_duplicates = grouped_df.count()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    count = {
+        'nr_records': num_records,
+        'nr_columns': num_columns,
+        'nr_duplicates': num_duplicates
+    }
+
+    return count
+
+def min_max_mean_avg(df):
+
+    result = {}
+    min_suffix = "min_"
+    max_suffix = "max_"
+    mean_suffix = "mean_"
+    avg_suffix = "avg_"
+
+    for column in df.columns:
+        result[column] = df.\
+        select( F.min(column).alias(f"{min_suffix}{column}"),
+            F.max(column).alias(f"{max_suffix}{column}"),
+            F.mean(column).alias(f"{mean_suffix}{column}"),
+            F.avg(column).alias(f"{avg_suffix}{column}")).collect()[0]
+   
+    # print(result)
+    return result
+
+def lengthStatics(df):
+    
+    column_stats = {}
+    for column in df.columns:
+        column_stats[column] = df.select(
+            min(length(column)).alias('min_length'),
+            max(length(column)).alias('max_length'),
+            avg(length(column)).alias('avg_length')
+        ).first()    
+
+    # print(column_stats)
+    return column_stats 
+
+def stdValue(df):    
+    std_dev={}
+    for column in df.columns:
+        std_dev[column] = df.select(stddev(column)).first()[0]
+
+    # print(std_dev)
+    return std_dev
+
+def unique_value(df):
+    
+    unique={}
+    for column in df.columns:
+        unique[column] = df.select(approx_count_distinct(column)).first()[0]
+
+    # print(unique)
+    return unique
+
+def dataType(df):
+
+    for column in df.columns:
+        data_type=str(df.schema[column].dataType)    
+
+    # print(data_type)
+    return(data_type)  
+
+def frequencyAnalysis(df):
+
+    frequency_Analysis = {}
+    for column in df.columns:
+        frequency_analysis = df.groupBy(column).agg(F.count("*").alias("count")).collect()
+        frequency_Analysis[column] = [(row[0], row[1]) for row in frequency_analysis]
+
+    # print(frequency_Analysis)
+    return frequency_Analysis 
+
+def getColumnDetail(df):
+    result = {}
+
+    for column in df.columns:
+        result[column] = df.select(F.min(column).alias('min_value'),
+            F.max(column).alias('max_value'),
+            F.mean(column).alias('mean_value'),
+            F.avg(column).alias('avg_value'),
+            min(length(column)).alias('min_length'),
+            max(length(column)).alias('max_length'),
+            avg(length(column)).alias('avg_length'),
+            stddev(column).alias('std_dev'),
+            approx_count_distinct(column).alias('unique')).first()
+        #result[column]['dataType'] = df.schema[column].dataType
+    return result
+ 
+if __name__ == '__main__' :
+    profile_api.run(debug=True)
 
 
 
